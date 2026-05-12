2b2c23219d9c7790ec8edff2f4d5b7c40f51e298
Andy DwiCahya Hidayantod <andydch@koidigital.co.id>
2026-05-12 13:49:57 +0700

    pasang deteksi query lemot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
+use Google\Client;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
 use Log;
 use Storage;
-use Google\Client;
-use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,7 +27,43 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->BackupToGoogleDrive();
+        // $this->BackupToGoogleDrive();
+
+        // if (!app()->isProduction()) {
+            // Mendengarkan semua query database yang dieksekusi
+            DB::listen(function ($query) {
+                // Properti $query->time menggunakan satuan milidetik (ms)
+                // 500 ms = 0.5 detik
+                // 5000 ms = 5 detik
+                if ($query->time > 5000) {
+                    // 1. Ambil riwayat pemanggilan fungsi (backtrace)
+                    // Menggunakan DEBUG_BACKTRACE_IGNORE_ARGS agar tidak memakan banyak memori
+                    $traces = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+                    $queryLocation = 'Lokasi tidak ditemukan';
+    
+                    // 2. Loop riwayat tersebut untuk mencari file pertama di luar folder 'vendor'
+                    foreach ($traces as $trace) {
+                        if (isset($trace['file']) && !str_contains($trace['file'], 'vendor' . DIRECTORY_SEPARATOR)) {
+                            // Jika menemukan file aplikasi kita (misal: app/Http/Controllers/UserController.php)
+                            $queryLocation = $trace['file'] . ' pada baris ' . $trace['line'];
+                            break; // Hentikan loop karena lokasi asal sudah ditemukan
+                        }
+                    }
+                    
+                    Log::warning('Slow Query Detected', [
+                        // 'sql' => $query->sql,
+                        // 'bindings' => $query->bindings,
+                        'time' => $query->time . ' ms',
+                        'connection' => $query->connectionName,
+                        'location' => $queryLocation,
+                    ]);
+                    Log::warning('SQL Statement', [
+                        'sql' => $query->sql,
+                        'bindings' => $query->bindings,
+                    ]);
+                }
+            });
+        // }
     }
 
     private function BackupToGoogleDrive(){
