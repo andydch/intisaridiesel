4b7508f4125ab3e21e8486704d08f07b92d98914
Andy DwiCahya Hidayantod <andydch@koidigital.co.id>
2026-08-12 17:23:47 +0700

    update helper globalfunctionhelper

--- app/Helpers/GlobalFuncHelper.php
+++ app/Helpers/GlobalFuncHelper.php
@@ -6,8 +6,11 @@ class GlobalFuncHelper
 {
     public static function moneyValidate($val)
     {
-        $val = str_replace(",", "", $val);
-        return $val;
+        $val = trim(str_replace(",", "", $val));
+        // Nilai kosong/non-numerik dikembalikan sebagai 0 (int), bukan string ""
+        // agar aman dipakai di floor()/round() (PHP 8 menolak string non-numerik)
+        // dan tidak mengubah nilai yang valid.
+        return $val === '' ? 0 : $val;
     }
 
     // public function startQueryLog()
